27f21f50536426ef8e534cf2251e2869ba1d1295
joonjCode <jtj308@naver.com>
2021-02-06 01:09:42 +0900

    Apollo and react hook done

--- src/components/posts/posts-form/posts-form.components.tsx
+++ src/components/posts/posts-form/posts-form.components.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useCreatePost } from '../../../hooks/posts/useCreatePost';
 
 interface FormData{
     title:string;
@@ -7,10 +8,18 @@ interface FormData{
 }
 
 const PostsForm:React.FC = () =>{
-    const {register, handleSubmit} = useForm<FormData>()
+    const {register, handleSubmit} = useForm<FormData>();
+    const createPost = useCreatePost();
 
     const onSubmit = handleSubmit(({title, body})=>{
-
+        createPost({
+            variables: {
+                input: {
+                    title,
+                    body
+                }
+            }
+        })
     });
     return (
         <div className="posts-form">
